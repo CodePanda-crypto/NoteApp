be9424a5eea434ec3167c23e810917fd7ece77c7
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-24 13:43:36 +0500

    Light and Dark themes for the app

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,26 +1,35 @@
 import './header.css';
 import appLogo from '/images/cute-halloween-skeleton.png';
+import { useTheme } from '../ThemeContext';
+import ToggleSwitch from './ToggleSwitch';
 
 export default function Header() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <header className="App-header">
       <nav className="navbar">
-        <div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={appLogo}
-              className="App-logo react logo-spin"
-              alt="React logo"
-            />
-          </a>
+        <div className="logo-title-container">
+          <div>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={appLogo}
+                className="App-logo react logo-spin"
+                alt="React logo"
+              />
+            </a>
+          </div>
+          <div>
+            <h1 className="App-title">Meme Generator</h1>
+          </div>
         </div>
-        <div>
-          <h1 className="App-title">Meme Generator</h1>
+        <div className="toggle-theme-container">
+          <ToggleSwitch isOn={theme === 'dark'} handleToggle={toggleTheme} />
         </div>
       </nav>
     </header>
